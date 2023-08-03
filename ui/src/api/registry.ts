6de5cb8975b02659633ead792db1ac0709aee4d7
jenya <hypnagonia>
2023-08-03 00:18:39 -0300

    registry

--- ui/src/api/registry.ts
+++ ui/src/api/registry.ts
@@ -4,19 +4,21 @@ import SignClient from '@walletconnect/sign-client'
 
 //more rpc providers
 //https://rpc.info/#goerli
-export const rpcUrl = 'https://rpc.ankr.com/eth_goerli'
-const chainId = 5
-const registryAddess = '0x4936d1f3068f8eC36eCbEd39DbA0395b87f4Ce40'
+export const rpcUrl = process.env.REACT_APP_PROVIDER_URL || 'https://rpc.ankr.com/eth_goerli'
+// const chainId = 5
+const registryAddess = process.env.REACT_APP_SNAPS_REGISTRY_ADDRESS || ''
 
-export const provider = new ethers.JsonRpcProvider(rpcUrl, chainId)
 
-const registry = new ethers.Contract(registryAddess, registryABIJSON, provider)
+export const provider = new ethers.JsonRpcProvider(rpcUrl)
 
+const registry = new ethers.Contract(registryAddess, registryABIJSON.abi, provider)
+
+const registryKey = 'snaps-registry-' + registryAddess
 
 export const getSnaps = async () => {
 
     try {
-        const cached = JSON.parse(localStorage.getItem('registry') as any)
+        const cached = JSON.parse(localStorage.getItem(registryKey) as any)
         console.log({ cached })
 
         if (cached) {
@@ -32,31 +34,39 @@ export const getSnaps = async () => {
             : value
     )))
 
-    localStorage.setItem('registry', JSON.stringify(r))
+    localStorage.setItem(registryKey, JSON.stringify(r))
     console.log({ r, res })
     return r
 }
 
 
 export const getSnapData = async () => {
-    const count = await registry.snapsIndex()
+    console.log('getting snaps')
+    let count
+    try {
+        // count = await registry.snapsIndex()
+        count = await registry.getSnapTotalSupply()//.then(r => r.toString())
+    } catch (e) {
+        count = await registry.getSnapTotalSupply()//.then(r => r.toString())
+    }
+    console.log('snaps total', count)
     const snaps = {} as any
     for (let i = 1; i <= count; i++) {
         snaps[i] = {} as any
-        const meta = await registry.snaps(i)
-
+        const meta = await registry.getSnap('' + i)
+        console.log('getSnap ' + i, meta)
         snaps[i].meta = meta
 
     }
 
     for (let i = 1; i <= count; i++) {
-        const versions = Object.values(await registry.getSnapReleasedVersions(i)) as string[]
-
+        const versions = Object.values(await registry.getSnapVersions(i)) as string[]
+        console.log('getSnapVersions ' + i, versions)
         snaps[i].versionList = versions
         snaps[i].versions = {} as any
 
         for (let v of versions) {
-            const versionData = await registry.getSnapVersion(i, v)
+            const versionData = await registry.getSnapVersion(v)
             snaps[i].versions[v] = versionData
         }
     }
